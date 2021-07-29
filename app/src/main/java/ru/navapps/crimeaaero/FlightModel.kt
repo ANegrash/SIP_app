bd6b317c610fc3c9f64a3f085bf31ea592c31bc7
season6 <season.personal@protonmail.com>
2021-07-29 09:19:51 +0300

    Redo null`s in FlightModel

--- app/src/main/java/ru/navapps/crimeaaero/FlightModel.kt
+++ app/src/main/java/ru/navapps/crimeaaero/FlightModel.kt
@@ -1,20 +1,23 @@
 package ru.navapps.crimeaaero
 
-import java.util.*
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDate
 
-class FlightModel (val id: Int){
-    var type: Int? = null //0 - all, 1 - departure, 2 - arrival
-    var dateTime: Date? = null
-    var airlineAbbr: String? = null
-    var airlineName: String? = null
-    var airlineLogoPath: String? = null
-    var flightNumber: Int? = null
-    var flightNumberString: String? = null
-    var checkinRow: String? = null
-    var terminal: Int? = null
-    var gate: String? = null
-    var airport: String? = null
-    var direction: String? = null
-    var status: String? = null
-    var delay: String? = null
+data class FlightModel (val id: Int){
+    var type: Int = -1 //0 - all, 1 - departure, 2 - arrival
+    @RequiresApi(Build.VERSION_CODES.O)
+    var dateTime: LocalDate = LocalDate.of(0,0,0)
+    var airlineAbbr: String = ""
+    var airlineName: String = ""
+    var airlineLogoPath: String = ""
+    var flightNumber: Int = -1
+    var flightNumberString: String = ""
+    var checkinRow: String = ""
+    var terminal: Int = -1
+    var gate: String = ""
+    var airport: String = ""
+    var direction: String = ""
+    var status: String = ""
+    var delay: String = ""
 }
