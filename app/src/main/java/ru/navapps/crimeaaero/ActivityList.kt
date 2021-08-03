f876a22141e1369ab3e9cab5f4793f3e785e1d97
Andrey Negrash <andi.ne2002@gmail.com>
2021-08-03 22:17:00 +0300

    Selection date and direction works good

--- app/src/main/java/ru/navapps/crimeaaero/ActivityList.kt
+++ app/src/main/java/ru/navapps/crimeaaero/ActivityList.kt
@@ -1,7 +1,8 @@
 package ru.navapps.crimeaaero
 
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,11 +16,78 @@ import java.io.IOException
 
 class ActivityList : AppCompatActivity() {
 
+    var datesArray = arrayOf("Вчера", "Сегодня", "Завтра")
+    var datesArrayQuery = arrayOf("yesterday", "today", "tomorrow")
+    var dateVar = 0
+
+    var typesArray = arrayOf("Вылет", "Прилёт")
+    var typesArrayQuery = arrayOf("departure", "arrival")
+    var typeVar = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
-        val url = getJsonFromApi("departure", "yesterday")
+        val spinnerDates = findViewById<View>(R.id.spinner_dates) as Spinner
+        val adapterDates: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datesArray)
+        adapterDates.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerDates.adapter = adapterDates
+
+        val spinnerTypes = findViewById<View>(R.id.spinner_flightTypes) as Spinner
+        val adapterTypes: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, typesArray)
+        adapterTypes.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerTypes.adapter = adapterTypes
+
+        val reloadBtn = findViewById<View>(R.id.btn_reloadTable) as ImageButton
+
+        reloadBtn.setOnClickListener {
+            setRecyclerViewContent(typesArrayQuery[typeVar], datesArrayQuery[dateVar])
+        }
+
+        val itemSelectedListenerDates: AdapterView.OnItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    dateVar = position
+                    setRecyclerViewContent(typesArrayQuery[typeVar], datesArrayQuery[dateVar])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+        spinnerDates.onItemSelectedListener = itemSelectedListenerDates
+
+        val itemSelectedListenerTypes: AdapterView.OnItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    typeVar = position
+                    setRecyclerViewContent(typesArrayQuery[typeVar], datesArrayQuery[dateVar])
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+        spinnerTypes.onItemSelectedListener = itemSelectedListenerTypes
+
+        setRecyclerViewContent("departure", "tomorrow")
+
+    }
+
+    private fun setRecyclerViewContent(
+        type : String,
+        date : String,
+        lang: String = "ru") {
+
+        var url = "https://new.sipaero.ru/json/schedule/?"
+        if (type != "") url += "type=$type&"
+        if (date != "") url += "date=$date&"
+        url += "lang=$lang&"
 
         val test = Get()
 
@@ -39,11 +107,41 @@ class ActivityList : AppCompatActivity() {
                 @Throws(IOException::class)
                 override fun onResponse(call: Call, response: Response) {
                     val listOfObjects: ArrayList<FlightModel> = arrayListOf()
+                    listOfObjects.clear()
                     val responseString = response.body()!!.string()
                     val json : JsonObject? = Gson().fromJson(responseString, JsonObject::class.java)
+
                     if (json != null) {
-                        for (i in json.departure?.yesterday!!) {
-                            listOfObjects.add(i)
+                        if (type == "departure"){
+                            when (date) {
+                                "yesterday" -> {
+                                    for (i in json.departure?.yesterday!!)
+                                        listOfObjects.add(i)
+                                }
+                                "today" -> {
+                                    for (i in json.departure?.today!!)
+                                        listOfObjects.add(i)
+                                }
+                                "tomorrow" -> {
+                                    for (i in json.departure?.tomorrow!!)
+                                        listOfObjects.add(i)
+                                }
+                            }
+                        } else if (type == "arrival"){
+                            when (date) {
+                                "yesterday" -> {
+                                    for (i in json.arrival?.yesterday!!)
+                                        listOfObjects.add(i)
+                                }
+                                "today" -> {
+                                    for (i in json.arrival?.today!!)
+                                        listOfObjects.add(i)
+                                }
+                                "tomorrow" -> {
+                                    for (i in json.arrival?.tomorrow!!)
+                                        listOfObjects.add(i)
+                                }
+                            }
                         }
                     }
 
@@ -63,7 +161,6 @@ class ActivityList : AppCompatActivity() {
                 }
             }
         )
-
     }
 
 
@@ -71,17 +168,5 @@ class ActivityList : AppCompatActivity() {
         return ((date.split("T").toTypedArray()[1]).split("+").toTypedArray()[0]).split(":")
             .toTypedArray()[0] + ":" + ((date.split("T").toTypedArray()[1]).split("+")
             .toTypedArray()[0]).split(":").toTypedArray()[1]
-    }
-
-    private fun getJsonFromApi(
-        typeQuery: String?,
-        dateQuery: String?,
-        langQuery: String = "ru"
-    ): String {
-        var url = "https://new.sipaero.ru/json/schedule/?"
-        if (typeQuery != "") url += "type=$typeQuery&"
-        if (dateQuery != "") url += "date=$dateQuery&"
-        url += "lang=$langQuery&"
-        return url
     }
 }
